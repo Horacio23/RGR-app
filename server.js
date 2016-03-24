b441509024a8f407b3a2bf10a73bfeb68d851f2c
horacio23 <horacio.delgado23@gmail.com>
2016-03-24 19:59:34 -0400

    project setup is completed, a simple flux flow has been set together with a mongo database connection

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-var express = require('express');
+express = require('express');
+MongoClient = require('mongodb');
 
 var app = express();
 
-
+// ES-2015 Syntax: Arrow Functions
+//app.get('/', (req, res) => res.send('helasdlo express'));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.send('hello express');
-// });
 
-app.listen(3000);
+
+var db;
+//Future code
+//mongodb://Ikanant:apple@ds021999.mlab.com:21999/rgrjs
+MongoClient.connect("mongodb://localhost:27017/rgrjs", (err, database) => {
+    if (err) throw err;
+
+    db = database;
+    app.listen(3000, () => {console.log("Listening on port 3000")});
+});
+
+app.get('/data/links', (req, res) => {
+  db.collection("links").find({}).toArray((err, links) => {
+      if(err) throw err;
+      console.log(links);
+      res.json(links);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 var options = {
   entry: "./js/app.js",
   output: {
