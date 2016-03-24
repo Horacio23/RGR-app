2887f18f4dff64a3f710bfc77774d427cb86321f
horacio23 <horacio.delgado23@gmail.com>
2016-03-23 22:25:05 -0400

    finally made the project with with babel, ty Jonathan

---
+++ js/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render() {
+      return (
+        <h3>Hello</h3>
+      );
+    }
+}
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+
+var options = {
+  entry: "./js/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  // Loaders: We will preprocess files before bundling them
+  module: {
+    // Array of loaders
+    loaders: [
+      // test: anything that ends in .js
+      // I was getting errors so I had to do: npm install babel-preset-react and add a query here
+      { test: /\.js$/, loader: 'babel-loader', query:{presets:['react', 'es2015']} }
+    ]
+  }
+}
+
+module.exports = options
