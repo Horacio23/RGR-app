1309f5e2948e429ec0dcc623905159fdf717baae
horacio23 <horacio.delgado23@gmail.com>
2016-03-25 19:00:44 -0400

    Welp, things went better than expected. The index.js file is no longer required since i am no using the presets in the npm start script

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import graphQLHTTP from 'express-graphql';
@@ -10,28 +8,16 @@ const app = express();
 // app.get('/', (req, res) => res.send('helasdlo express'));
 app.use(express.static('public'));
 
-let db;
-// Future code
-// mongodb://Ikanant:apple@ds021999.mlab.com:21999/rgrjs
-MongoClient.connect('mongodb://localhost:27017/rgrjs', (err, database) => {
-    if (err) throw err;
+( async () => {
+  let db = await MongoClient.connect('mongodb://localhost:27017/rgrjs');
 
-    db = database;
+  app.use('/graphql', graphQLHTTP({
+      schema: schema(db),
+      graphiql: true,
+  }));
 
-    app.use('/graphql', graphQLHTTP({
-        schema: schema(db),
-        graphiql: true,
-    }));
+  app.listen(3000, () => {
+      console.log('Listening on port 3000');
+  });
 
-    app.listen(3000, () => {
-        console.log('Listening on port 3000');
-    });
-});
-
-app.get('/data/links', (req, res) => {
-    db.collection('links').find({}).toArray((err, links) => {
-        if (err) throw err;
-        console.log(links);
-        res.json(links);
-    });
-});
+})();
