a93bcdd48e2f759f6c654c829a8fd0cb66bcb4a9
horacio23 <horacio.delgado23@gmail.com>
2016-03-25 20:28:54 -0400

    Relay has been successfully set up, with babel plugin and everything. Things are going well

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import fs from 'fs';
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import graphQLHTTP from 'express-graphql';
-import schema from './data/schema';
+import Schema from './data/schema';
+import { graphql } from 'graphql';
+import { introspectionQuery } from 'graphql/utilities';
+
 const app = express();
 
 // ES-2015 Syntax: Arrow Functions
@@ -10,9 +14,10 @@ app.use(express.static('public'));
 
 ( async () => {
   let db = await MongoClient.connect('mongodb://localhost:27017/rgrjs');
+  let schema = Schema(db);
 
   app.use('/graphql', graphQLHTTP({
-      schema: schema(db),
+      schema,
       graphiql: true,
   }));
 
@@ -20,4 +25,12 @@ app.use(express.static('public'));
       console.log('Listening on port 3000');
   });
 
+  // This doesnt have to be here since it will be generated every time the server runs but w.e
+  // Generate schema.json for Relay.
+  const json = await graphql(schema, introspectionQuery)
+  fs.writeFile('./data/schema.json', JSON.stringify(json, null, 2), err => {
+    if (err) throw err;
+
+    console.log('JSON schema created');
+  });
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var babelRelayPlugin = __dirname+'/babelRelayPlugin';
+
 var options = {
   entry: "./js/app.js",
   output: {
-    path: __dirname + "/public",
+    path: __dirname + "/public/js",
     filename: "bundle.js"
   },
   // Loaders: We will preprocess files before bundling them
@@ -10,9 +12,15 @@ var options = {
     loaders: [
       // test: anything that ends in .js
       // I was getting errors so I had to do: npm install babel-preset-react and add a query here
-      { test: /\.js$/, loader: 'babel-loader', query:{presets:['react', 'es2015', 'stage-0']} }
-    ]
-  }
-}
+        {
+            test: /\.js$/, loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+                plugins: [babelRelayPlugin],
+            },
+        },
+    ],
+  },
+};
 
-module.exports = options
+module.exports = options;
